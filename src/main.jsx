f68689f4d133cd5b419a0f14f70746dec624dce1
Toh Jun Sheng <20junsheng01@gmail.com>
2025-07-15 02:32:40 +0800

    add Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
